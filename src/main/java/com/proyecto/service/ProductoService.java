589c25d217f99e6695cc8bd5d015c1282ac5fcb9
joseph072 <i201910034@cibertec.edu.pe>
2021-11-20 16:04:20 -0500

    registro producto

---
+++ src/main/java/com/proyecto/service/ProductoService.java
@@ -0,0 +1,13 @@
+package com.proyecto.service;
+
+import java.util.Optional;
+
+import com.proyecto.model.Producto;
+
+public interface ProductoService {
+	public Producto save(Producto producto);
+	public Optional<Producto> get(Integer id);
+	public void update(Producto producto);
+	public void delete(Integer id);
+	
+}
